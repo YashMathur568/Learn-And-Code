9c9d25c0729f82c11886dc396af33d249d930528
yashmathur865 <yashmathur865@gmail.com>
2026-04-29 14:21:38 +0530

    Commit 3: Multiply function

--- Git Practice/Git Bisect/calculator.cpp
+++ Git Practice/Git Bisect/calculator.cpp
@@ -8,6 +8,10 @@ int subtract(int a, int b) {
     return a - b;
 }
 
+int multiply(int a, int b) {
+    return a + b;
+}
+
 int main() {
     std::cout << "Calculator Program" << std::endl;
     return 0;
