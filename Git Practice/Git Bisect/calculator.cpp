1cad499a8f632dc695c737a889fc030d0db820c1
yashmathur865 <yashmathur865@gmail.com>
2026-04-29 14:21:30 +0530

    Commit 2: Subtract function

--- Git Practice/Git Bisect/calculator.cpp
+++ Git Practice/Git Bisect/calculator.cpp
@@ -4,6 +4,10 @@ int add(int a, int b) {
     return a + b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     std::cout << "Calculator Program" << std::endl;
     return 0;
